09647c7af31553bdd2d9bd753e92694cc46459a9
Probably-Idiot <zadokfernando@gmail.com>
2023-06-20 19:30:50 +0100

    Tuesday evening commit - NOT WORKING

---
+++ Scenes/world.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+
+const Player = preload("res://Scenes/player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_host_btn_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	
+	add_player(multiplayer.get_unique_id())
+
+func _on_join_btn_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
